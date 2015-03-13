1ba7d8db94c852316c9ccc1a97d6a58161b6a19d
Philipp Anders <philipp.anders@sandstorm-media.de>
2015-03-13 20:05:59 +0100

    added map, changed colors

--- app/helpers/get-state.js
+++ app/helpers/get-state.js
@@ -2,5 +2,5 @@ import Ember from "ember";
 import StateEnum from '../models/stateEnum';
 
 export default Ember.Handlebars.makeBoundHelper(function(city, state) {
-  return "<div class='event-header state-" + state + "'>" + city + " / " + StateEnum[state] + "</div>";
+  return "<div class='event-header state-" + state + "'>" + city + "<br><b>" + StateEnum[state] + "</b></div>";
 });
