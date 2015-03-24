334abcaef8db02f70c2aba9ec8ddd2267efa73ed
Philipp Anders <philipp.anders@sandstorm-media.de>
2015-03-24 18:27:00 +0100

    rebuilt filter functionality

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -1,4 +1,56 @@
 import Ember from 'ember';
+import StateEnum from '../models/stateEnum';
+
 
 export default Ember.ArrayController.extend({
+    queryParams: ['state'],
+    state: [],
+    filteredEvents: Ember.computed(
+        'state',
+        function () {
+            var state = this.get('state');
+            if (state.length > 0) {
+
+                return this.get('model').filter(function (event) {
+                    var returnValue = false;
+                    state.forEach(function (currentState) {
+
+                        returnValue = returnValue || (event.get('state') == currentState);
+
+                    });
+                    return returnValue;
+                });
+
+            } else {
+                return this.get('model');
+            }
+        }),
+    manageStateClasses : function(state){
+
+        if(this.get('state').length == 0){
+            // no query parameters, remove all incactive
+            for (var key in StateEnum) {
+                this.set(key, true);
+            }
+        } else {
+            // query parameters, make everything inactive
+            for (var key in StateEnum) {
+                if(this.get('state').contains(key)){
+                    this.set(key, true);
+                } else {
+                    this.set(key, false);
+                }
+            }
+        }
+    }.on('init').observes('state.[]'),
+    actions: {
+        setState: function (state) {
+            if (this.get('state').contains(state)) {
+                this.get('state').removeObject(state);
+            } else {
+                this.get('state').pushObject(state);
+            }
+        }
+    }
+
 });
